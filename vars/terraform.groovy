a5f0d64f9a9b11d2e79115c42fdb9bd73d26d31b
manoj <gandhamanojkutty@gmail.com>
2024-03-27 18:10:34 +0530

    code updated

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -1,35 +1,38 @@
-pipeline {
+def call(int buildNumber) {
+    pipeline {
 
-    agent {
-        node {
-            label 'workstation'
-        }
-    }
-
-    parameters {
-        choice(name: 'env', choices: ['dev', 'prod'], description: 'Pick environment')
-    }
-
-    stages {
-
-        stage('Terraform Init') {
-            steps {
-                sh 'terraform init -backend-config=env-${env}/state.tfvars'
+        agent {
+            node {
+                label 'workstation'
             }
         }
 
-        stage('Terraform Apply') {
-            steps {
-                sh 'terraform apply -auto-approve -var-file=env-${env}/main.tfvars'
+        parameters {
+            choice(name: 'env', choices: ['dev', 'prod'], description: 'Pick environment')
+        }
+
+        stages {
+
+            stage('Terraform Init') {
+                steps {
+                    sh 'terraform init -backend-config=env-${env}/state.tfvars'
+                }
+            }
+
+            stage('Terraform Apply') {
+                steps {
+                    sh 'terraform apply -auto-approve -var-file=env-${env}/main.tfvars'
+                }
+            }
+
+        }
+
+        post {
+            always {
+                cleanWs()
             }
         }
 
     }
-
-    post {
-        always {
-            cleanWs()
-        }
-    }
-
 }
+
