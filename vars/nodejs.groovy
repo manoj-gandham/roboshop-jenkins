8423f8d28345f88d08d3517d049ba760362063c4
manoj <gandhamanojkutty@gmail.com>
2024-03-28 13:43:57 +0530

    code updated

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -16,13 +16,15 @@ def call () {
 
             stage('code Quality') {
                 steps {
-                    sh 'sonar-scanner -Dsonar.projectKey=${component} -Dsonar.host.url=http://172.31.91.92:9000 -Dsonar.login=admin -Dsonar.password=admin123'
+                   // sh 'sonar-scanner -Dsonar.projectKey=${component} -Dsonar.host.url=http://172.31.91.92:9000 -Dsonar.login=admin -Dsonar.password=admin123'
+                    sh 'echo COde Quality'
                 }
             }
 
             stage('unit test cases') {
                 steps {
-                    sh 'echo unit test cases'
+                    //sh 'echo unit test cases'
+                    sh 'npm run test'
                 }
             }
 
