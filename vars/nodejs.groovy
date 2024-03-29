9bf17207ede7a62c19848b532b3f8f1903643e13
manoj <gandhamanojkutty@gmail.com>
2024-03-29 23:28:34 +0530

    code updated

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -48,8 +48,9 @@ def call () {
                 }
                 steps {
                     sh 'npm install'
-
-                    sh 'curl -v -u admin:admin123 --upload-file server.js http://172.31.85.41:8081/repository/cart/server.js'
+                    sh 'echo $TAG_NAME >VERSION'
+                    sh 'zip -r ${component}-${TAG_NAME}.zip node_module server.js VERSION'
+                    sh 'curl -v -u admin:admin123 --upload-file ${component}-${TAG_NAME}.zip http://172.31.85.41:8081/repository/${component}/${component}-${TAG_NAME}.zip'
                 }
             }
 
