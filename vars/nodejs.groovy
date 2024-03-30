a83b556829376139cb16b407ab1bd57ff5203da7
manoj <gandhamanojkutty@gmail.com>
2024-03-30 10:30:09 +0530

    code updated

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -11,6 +11,10 @@ def call () {
             ansiColor('xterm')
         }
 
+        environment {
+            NEXUS = credentials('nexus')
+        }
+
 
         stages {
 
@@ -50,7 +54,7 @@ def call () {
                     sh 'npm install'
                     sh 'echo $TAG_NAME >VERSION'
                     sh 'zip -r ${component}-${TAG_NAME}.zip node_modules server.js VERSION'
-                    sh 'curl -v -u admin:admin123 --upload-file ${component}-${TAG_NAME}.zip http://172.31.85.41:8081/repository/${component}/${component}-${TAG_NAME}.zip'
+                    sh 'curl -v -u ${nexus_USR}:${nexus_PSW} --upload-file ${component}-${TAG_NAME}.zip http://172.31.85.41:8081/repository/${component}/${component}-${TAG_NAME}.zip'
                 }
             }
 
